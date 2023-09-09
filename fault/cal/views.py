9a268dfc2e8c2c838f2f7c5b3747b48ab07fdd8a
jin <qudwls831@gmail.com>
2023-09-09 20:45:08 +0900

    disable field 처리

--- fault/cal/views.py
+++ fault/cal/views.py
@@ -10,26 +10,44 @@ def manual(request):
 
 def fault_con(request):
     if request.method == 'POST':
-        new_con = FaultCondition(
-            basemva = request.POST['basemva'],
-            is_flow = request.POST['is_flow'],
-            fault_type = request.POST['fault_type'],
-            is_bus_fault = request.POST['is_bus_fault'],
-            fault_bus = request.POST['fault_bus'],
-            fault_line_1 = request.POST['fault_line_1'],
-            fault_line_2 = request.POST['fault_line_2'],
-            line_percentage = request.POST['line_percentage'],
-            impedence_R = request.POST['impedence_R'],
-            impedence_X = request.POST['impedence_X'],
-        )
+        basemva = request.POST['basemva']
+        is_flow = request.POST['is_flow']
+        fault_type = request.POST['fault_type']
+        is_bus_fault = request.POST['is_bus_fault']
+        impedence_R = request.POST['impedence_R']
+        impedence_X = request.POST['impedence_X']
+        if is_bus_fault:
+            fault_bus = request.POST['fault_bus']
+            fault_line_1 = 0
+            fault_line_2 = 0
+            line_percentage = 0
+        else:
+            fault_bus = 0
+            fault_line_1 = request.POST['fault_line_1']
+            fault_line_2 = request.POST['fault_line_2']
+            line_percentage = request.POST['line_percentage']
         if len(request.POST.getlist('is_shunt')):
-            FaultCondition.is_shunt = True
+            is_shunt = True
         else:
-            FaultCondition.is_shunt = False
+            is_shunt = False
         if len(request.POST.getlist('is_load_effect')):
-            FaultCondition.is_load_effect = True
+            is_load_effect = True
         else:
-            FaultCondition.is_load_effect = False    
+            is_load_effect = False
+        new_con = FaultCondition(
+            basemva = basemva,
+            is_flow = is_flow,
+            fault_type = fault_type,
+            is_bus_fault = is_bus_fault,
+            fault_bus = fault_bus,
+            fault_line_1 = fault_line_1,
+            fault_line_2 = fault_line_2,
+            line_percentage = line_percentage,
+            impedence_R = impedence_R,
+            impedence_X = impedence_X,
+            is_shunt =is_shunt,
+            is_load_effect = is_load_effect
+        )
         new_con.save()
         return redirect('main')
     return render(request, 'cal/fault_con.html')
