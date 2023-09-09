679be4f988d87200301810978b19fcc52b86c6ce
jin <qudwls831@gmail.com>
2023-09-09 16:54:46 +0900

    버튼 작동

--- fault/cal/static/cal/js/style.js
+++ fault/cal/static/cal/js/style.js
@@ -1,19 +1,19 @@
 function linefieldsetDisable()  {
-  const fieldset = document.getElementById('line_fieldset');
+  const fieldset = document.getElementById('linefieldset');
   fieldset.disabled = true;
 }
 
 function linefieldsetActive() {
-  const fieldset = document.getElementById('line_fieldset');
+  const fieldset = document.getElementById('linefieldset');
   fieldset.disabled = false;
 }
 
 function busActive()  {
-  const target = document.getElementById('id_fault_bus');
+  const target = document.getElementById('bus1');
   target.disabled = false;
 }
 
 function busDisable()  {
-  const target = document.getElementById('id_fault_bus');
+  const target = document.getElementById('bus1');
   target.disabled = true;
 }
